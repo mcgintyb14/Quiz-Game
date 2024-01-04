ef713c0eef2bb728d6e29a5f70f6831361f5785f
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-01-04 18:05:32 -0500

    finished setup of all files and defined the first array of dummy questions

---
+++ Assets/script.js
@@ -0,0 +1,33 @@
+  // Questions array with objects containing question, choices, and answer
+  // The third line is the Index of the correct answer in the choices array
+  const questions = [
+    {
+      question: 'What is the capital of France?',
+      choices: ['London', 'Paris', 'Rome', 'Madrid'],
+      answer: 1 
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      choices: ['Mars', 'Jupiter', 'Saturn', 'Uranus'],
+      answer: 0
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        choices: ['Mars', 'Jupiter', 'Saturn', 'Uranus'],
+        answer: 0
+      },
+      {
+        question: 'Which planet is known as the Red Planet?',
+        choices: ['Mars', 'Jupiter', 'Saturn', 'Uranus'],
+        answer: 0
+      },
+      {
+        question: 'Which planet is known as the Red Planet?',
+        choices: ['Mars', 'Jupiter', 'Saturn', 'Uranus'],
+        answer: 0
+      },
+  ];
+  
+
+  
+  
